4b2cbf0d393fea38b2b77edbf191da16c6291082
hemant1101 <hemantmandal1101@gmail.com>
2022-10-30 20:41:54 +0530

    hi

--- server.js
+++ server.js
@@ -5,15 +5,7 @@ require("./server/db/conn"); //for mongo
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoute = require("./server/router/user-router");
-// const AdminBroExpress = require("admin-bro-expressjs");
-// const AdminBro = require("admin-bro");
-// const options = require("./server/admin/admin-option");
-// const admin = new AdminBro(options);
-
-const adminLogin = {
-  email: process.env.adminLogin_email, // "root@admin.com",
-  password: process.env.adminLogin_password, // "toor123",
-};
+const { admin, router } = require("./server/admin/admin-option");
 // const router = AdminBroExpress.buildAuthenticatedRouter(admin, {
 //   authenticate: async (email, password) => {
 //     if (adminLogin.password === password && adminLogin.email === email) {
@@ -43,7 +35,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/", userRoute);
-// app.use(admin.options.rootPath, router);
+app.use(admin.options.rootPath, router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- server/admin/admin-option.js
+++ server/admin/admin-option.js
@@ -1,26 +1,47 @@
-const AdminBro = require("admin-bro");
-// const AdminBroSequelize = require("@admin-bro/sequelize");
-const User = require("../models/User");
-const Rooms = require("../models/Rooms");
-// AdminBro.registerAdapter(AdminBroSequelize);
-// const options = {
-//   resources: [User, Rooms],
-//   branding: {
-//     companyName: "Hotel Management System Admin",
-//     softwareBrothers: false,
-//     logo: "",
-//   },
-//   dashboard: { component: AdminBro.bundle("./ds") },
-//   locale: {
-//     translations: {
-//       messages: {
-//         loginWelcome: "Administration Panel - Login", // the smaller text
-//       },
-//       labels: {
-//         loginWelcome: "Hotel Management System", // this could be your project name
-//       },
-//     },
-//   },
-// };
+const adminjs = require("adminjs");
+const adminjsMongoose = require("@adminjs/mongoose");
+const adminjsExpress = require("@adminjs/express");
+adminjs.registerAdapter(adminjsMongoose);
 
-// module.exports = options;
+const Users = require("../models/User");
+const Rooms = require("../models/Rooms");
+
+const admin = new adminjs(
+    {
+        resources: [User, Rooms],
+        branding: {
+            companyName: "Hotel Management System Admin",
+            softwareBrothers: false,
+            logo: "",
+        },
+        locale: {
+            translations: {
+                messages: {
+                    loginWelcome: "Administration Panel - Login", // the smaller text
+                },
+                labels: {
+                    loginWelcome: "Hotel Management System", // this could be your project name
+                },
+            },
+        },
+    }
+);
+
+const router = adminjsExpress.buildAuthenticatedRouter(admin, {
+    authenticate: async (email, password) => {
+        if (
+            process.env.ADMIN_PASSWORD === password &&
+            process.env.ADMIN_EMAIL === email
+        ) {
+            return {
+                email: "LOL",
+                password: "1300135",
+            };
+        }
+        return null;
+    },
+    cookieName: "HotelTranselvania",
+    cookiePassword: "some-secret-password-used-to-secure-cookie",
+});
+
+module.exports = { router, admin };
